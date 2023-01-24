6135c8116175d208de6178efecc7abfcc6a0be06
kajal-2011 <kajalrekha888@gmail.com>
2023-01-24 21:52:35 +0600

    first commit

---
+++ queston2.js
@@ -0,0 +1,20 @@
+function binerySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (target === arr[mid]) {
+      return mid;
+    } else if (aee[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+console.log(binerySearch([2, 3, 5, 6, 8, 9], 3));
